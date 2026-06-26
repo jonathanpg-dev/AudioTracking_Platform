1e105268551eab70cc05760ed232ce23f97a332f
Jonathan Pierre Goho <jonathanpg56@gmail.com>
2026-06-25 23:13:08 -0400

    -Implemented POST /users endpoint (creating multiple users at once)

--- src/main/java/com/AudioTracking/Platform/Controllers/UserController.java
+++ src/main/java/com/AudioTracking/Platform/Controllers/UserController.java
@@ -8,14 +8,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UserController {
     @Autowired
     private UserRepo userRepo;
 
-    @PostMapping("api/v1/users")
+    @PostMapping("api/v1/user")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User response = userRepo.save(user);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("api/v1/users")
+    public List<User> createUsers(@RequestBody List<User> users) {
+        return userRepo.saveAll(users);
+    }
+
 }
